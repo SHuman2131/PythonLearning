86b4a71b9b095b04b5d5ccd1ec2e2ef7ed037081
Sergei <seregahardworking@yandex.ru>
2021-11-28 14:22:48 +0400

    Файлы, добавленные при создании проекта в PyCharm

---
+++ main.py
@@ -0,0 +1,12 @@
+class Animal:
+    name = ''
+    weight = 0
+
+    def feed(self):
+        print(f'{self.name} была покормлена')
+
+    def voice(self):
+        pass
+
+class Goose extends Animal:
+
